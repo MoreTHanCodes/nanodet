24b6f8164ae9856810c2528e064c878dd21e8e6a
Tao Han <tao.han@my.cityu.edu.hk>
2022-04-15 18:03:20 +0800

    Modification for custom training & inference

--- tools/test.py
+++ tools/test.py
@@ -74,7 +74,8 @@ def main(args):
     logger.info("Creating model...")
     task = TrainingTask(cfg, evaluator)
 
-    ckpt = torch.load(args.model)
+    # ckpt = torch.load(args.model)
+    ckpt = torch.load(args.model, map_location=lambda storage, loc: storage)
     if "pytorch-lightning_version" not in ckpt:
         warnings.warn(
             "Warning! Old .pth checkpoint is deprecated. "
